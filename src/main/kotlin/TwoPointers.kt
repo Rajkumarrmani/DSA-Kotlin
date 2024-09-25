c24e957d15ffaf6c0dc61c71725ab885c550c9a2
RaJManii <rajkumar.m@dellainfotech.com>
2024-09-25 17:55:13 +0530

    Two pointers - container pbm

--- src/main/kotlin/TwoPointers.kt
+++ src/main/kotlin/TwoPointers.kt
@@ -1,7 +1,11 @@
+
+import kotlin.math.max
+import kotlin.math.min
+
 fun main() {
-    val input = intArrayOf(-5, -2, 1, 8, 10, 12, 15)
-    val k = 1
-    println(findPairTwoPointers(input, k))
+    val input = intArrayOf(1, 5, 4, 3)
+    val k = 0
+    println(containerWithMostWater(input))
 }
 
 fun findAnyPair(input: IntArray, k: Int): Boolean {
@@ -19,15 +23,87 @@ fun findAnyPair(input: IntArray, k: Int): Boolean {
 fun findPairTwoPointers(input: IntArray, k: Int): Boolean {
 
     var start = 0
-    var end = input.size -1
+    var end = input.size - 1
 
-    while(start < end) {
+    while (start < end) {
         val sum = input[start] + input[end]
-        if(sum == k) return true
+        if (sum == k) return true
         else if (sum < k) start++
         else end--
     }
 
     return false
     // T.C - O(N) S.C - O(1)
+}
+
+fun countOfPairs(input: IntArray, k: Int): Int {
+
+    // case 1 - when elements are different
+
+    var count = 0
+
+    var start = 0
+    var end = input.size - 1
+
+    while (start < end) {
+        val sum = input[start] + input[end]
+        if (sum == k) {
+            count += 1
+            start++
+            end--
+        } else if (sum < k) start++
+        else end--
+    }
+
+    return count
+
+    // case 2 - when elements are duplicate
+    //TODO
+}
+
+fun pairDifference(input: IntArray, k: Int): Int {
+
+    input.sort()
+
+    var start = 0
+    var end = 1
+    var count = 0
+    val size = input.size
+
+    while (end < size) {
+        val diff = input[end] - input[start]
+        if (diff == k) {
+            count += 1
+            start += 1
+            end++
+        } else if (diff < k) {
+            end += 1
+        } else {
+            start += 1
+        }
+
+    }
+    return count
+}
+
+
+fun containerWithMostWater(input: IntArray): Int {
+
+    var output = Integer.MIN_VALUE
+    var start = 0
+    var end = input.size - 1
+
+    while (start < end) {
+        val diff = end - start
+        val min = min(input[start], input[end])
+        output = max(output, (diff * min))
+
+        if (input[start] < input[end]) start++
+        else if (input[start] > input[end]) end--
+        else {
+            start++
+            end--
+        }
+    }
+    return output
 }
