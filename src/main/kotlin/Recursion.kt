0f5cbdcd579a35f9c2712e5eb3fbb06082fad4ba
RaJManii <rajkumar.m@dellainfotech.com>
2024-10-02 11:20:28 +0530

    Recursion - Factorial

--- src/main/kotlin/Recursion.kt
+++ src/main/kotlin/Recursion.kt
@@ -1,9 +1,15 @@
 fun main() {
-    println(sumOfNaturalNumber(5))
+    println(factorial(5))
 }
 
 fun sumOfNaturalNumber(n: Int): Int {
     if (n == 1) return 1
     val sum = sumOfNaturalNumber(n - 1)
     return sum + n
+}
+
+fun factorial(n: Int) : Int {
+    if(n==1) return 1
+    val res = factorial(n-1)
+    return res * n
 }
